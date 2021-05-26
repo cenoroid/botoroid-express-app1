271281c57260a7a850c4e06a3575d0a988cfdd12
Cenoroid <almostwar@gmail.com>
2021-05-26 13:57:07 +0300

    1

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ dotenv.config();
 const MongoClient = require("mongodb").MongoClient;
 const { ObjectId } = require("bson");
 const app = express();
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 const server = require("http").createServer(app);
 const io = require("socket.io")(server, {
